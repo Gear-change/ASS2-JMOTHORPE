68ad99bf669fa7fab271a4533454bafe9b159d09
Gear-change <51209685+Gear-change@users.noreply.github.com>
2023-10-24 14:30:45 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const keys = document.querySelector('.learningarea')
+
+keys.addEventListener('click', (e) => {
+    // e.etarget.value|| e.target.name
+    const { target } = e;
+    const { value } = target;
+})
